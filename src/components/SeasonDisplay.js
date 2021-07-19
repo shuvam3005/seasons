9a715b2dcc3a2f832626de42b61d4e52f132afa5
Shuvam Guha <shuvam.g@apollo247.org>
2021-07-19 11:21:13 +0530

    Added props to SeasonDisplau

--- src/components/SeasonDisplay.js
+++ src/components/SeasonDisplay.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React from "react";
 
-const SeasonDisplay = ()=>{
-    return <div>Season display</div>
+const SeasonDisplay = (props) => {
+	return <div>Season display</div>;
 };
 
 export default SeasonDisplay;
